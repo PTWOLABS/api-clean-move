862124b851d7f7212813f2b2c2eae3b64a56ec4f
Pedrowill21 <pedrowilliam2999@gmail.com>
2026-05-05 09:50:03 -0300

    chore: update src/modules/application/repositories/employees-repository.ts
    Split previous aggregate change into file-level commit.

--- src/modules/application/repositories/employees-repository.ts
+++ src/modules/application/repositories/employees-repository.ts
@@ -4,5 +4,10 @@ import { Employee } from "../../employees/domain/entities/employee";
 @Injectable()
 export abstract class EmployeesRepository {
   abstract create(employee: Employee): Promise<void>;
+  abstract save(employee: Employee): Promise<void>;
   abstract findByUserId(userId: string): Promise<Employee | null>;
+  abstract findByIdAndEstablishmentId(
+    id: string,
+    establishmentId: string,
+  ): Promise<Employee | null>;
 }
