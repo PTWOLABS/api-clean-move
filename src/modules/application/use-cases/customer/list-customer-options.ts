bd128cd7ac34964673d24abfeede7abdddee5ce4
Felipe Pereira <fehpereira237@gmail.com>
2026-05-26 09:10:18 -0300

    feat: implement ListCustomerOptionsUseCase for retrieving customer options

---
+++ src/modules/application/use-cases/customer/list-customer-options.ts
@@ -0,0 +1,61 @@
+import { Injectable } from "@nestjs/common";
+
+import { Either, left, right } from "../../../../shared/either";
+import { NotAllowedError } from "../../../../shared/errors/not-allowed-error";
+import { ResourceNotFoundError } from "../../../../shared/errors/resource-not-found-error";
+import {
+  CustomerOption,
+  CustomersRepository,
+} from "../../repositories/customers-repository";
+import {
+  EstablishmentScopeActor,
+  EstablishmentScopeService,
+} from "../../services/establishment-scope";
+
+type ListCustomerOptionsUseCaseRequest = {
+  actor: EstablishmentScopeActor;
+  search?: string;
+  limit?: number;
+};
+
+type ListCustomerOptionsUseCaseResponse = Either<
+  ResourceNotFoundError | NotAllowedError,
+  {
+    customers: CustomerOption[];
+  }
+>;
+
+@Injectable()
+export class ListCustomerOptionsUseCase {
+  constructor(
+    private customersRepository: CustomersRepository,
+    private establishmentScope: EstablishmentScopeService,
+  ) {}
+
+  async execute({
+    actor,
+    search,
+    limit,
+  }: ListCustomerOptionsUseCaseRequest): Promise<ListCustomerOptionsUseCaseResponse> {
+    const scopeResult = await this.establishmentScope.resolve(actor);
+
+    if (scopeResult.isLeft()) {
+      return left(scopeResult.value);
+    }
+
+    const { establishment } = scopeResult.value;
+
+    const customers =
+      await this.customersRepository.findOptionsByEstablishmentId(
+        establishment.id.toString(),
+        {
+          ...(search !== undefined ? { search } : {}),
+          ...(limit !== undefined ? { limit } : {}),
+        },
+      );
+
+    return right({
+      customers,
+    });
+  }
+}
